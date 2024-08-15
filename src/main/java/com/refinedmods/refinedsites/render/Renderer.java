c17ebe502b1ee627dc5c5ff19f954ce4bd03750c
raoulvdberge <raoulvdberge@gmail.com>
2024-08-15 17:02:36 +0200

    feat: page types and articles

--- src/main/java/com/refinedmods/refinedsites/render/Renderer.java
+++ src/main/java/com/refinedmods/refinedsites/render/Renderer.java
@@ -18,8 +18,11 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -167,16 +170,29 @@ public class Renderer {
         );
         final PageAttributeCache pageAttributeCache = new PageAttributeCache();
         final Map<Path, PageInfo> pageInfo = new HashMap<>();
+        final Map<String, List<PageInfo>> infosByPageType = new HashMap<>();
         for (final Path pagePath : component.getPages()) {
-            pageInfo.put(pagePath, renderPagePre(
+            final PageInfo singleInfo = renderPagePre(
                 pagePath,
                 component,
                 pageAttributeCache,
                 sourceToDestinationAssets,
                 componentOutputPath
-            ));
+            );
+            pageInfo.put(pagePath, singleInfo);
+            infosByPageType.computeIfAbsent(singleInfo.type(), k -> new ArrayList<>()).add(singleInfo);
         }
         prepareNavigationItems(component.getNavigationItems(), pageInfo);
+        final List<ArticleRender> articles = infosByPageType.getOrDefault("article", Collections.emptyList())
+            .stream()
+            .map(info -> new ArticleRender(
+                info.title(),
+                info.description(),
+                info.relativePath(),
+                info.date().orElse(LocalDate.EPOCH)
+            ))
+            .sorted(Comparator.comparing(ArticleRender::getDate).reversed())
+            .toList();
         for (final Path pagePath : component.getPages()) {
             renderPage(
                 pagePath,
@@ -188,7 +204,8 @@ public class Renderer {
                 parsedReleases,
                 releaseMatchingComponentVersion,
                 sitemapBaseUrl,
-                componentSitemap
+                componentSitemap,
+                articles
             );
         }
         if (componentSitemap != null) {
@@ -366,7 +383,10 @@ public class Renderer {
                 .toList();
             final String parsedContent = (String) document.getContent();
             return PageInfo.builder()
+                .type(pageAttributes.type())
                 .title(pageAttributes.name())
+                .description(pageAttributes.description())
+                .date(pageAttributes.date())
                 .tableOfContents(toc)
                 .iconReferences(icons)
                 .parsedContent(parsedContent
@@ -397,7 +417,8 @@ public class Renderer {
                             final List<ParsedRelease> releases,
                             final ParsedRelease releaseMatchingComponentVersion,
                             final String baseUrl,
-                            @Nullable final WebSitemapGenerator sitemapGenerator) throws IOException {
+                            @Nullable final WebSitemapGenerator sitemapGenerator,
+                            final List<ArticleRender> articles) throws IOException {
         log.info("Rendering page {}", pagePath);
         final Context context = new Context();
         final PageInfo info = pageInfo.get(pagePath);
@@ -422,22 +443,29 @@ public class Renderer {
             .collect(Collectors.toList());
         Collections.reverse(otherReleases);
         context.setVariable("otherReleases", otherReleases);
+        context.setVariable("articles", articles);
+        context.setVariable("date", info.date().orElse(LocalDate.EPOCH));
+        context.setVariable("description", info.description());
         linkBuilder.setCurrentPageOutputPath(info.pageOutputPath());
-        final String template = getTemplate(pagePath);
+        final String template = getTemplate(pagePath, info.type());
         templateEngine.process(template, context, fileWriter);
         if (sitemapGenerator != null && !info.relativePath().contains("404")) {
             sitemapGenerator.addUrl(new WebSitemapUrl.Options(
                 baseUrl + "/" + componentOutputPath.relativize(info.pageOutputPath())
-            ).lastMod(renderDate).changeFreq(ChangeFreq.DAILY).build());
+            ).lastMod(info.date().map(Renderer::asDate).orElse(renderDate)).changeFreq(ChangeFreq.DAILY).build());
         }
     }
 
-    private String getTemplate(final Path pagePath) {
+    private static Date asDate(final LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private String getTemplate(final Path pagePath, final String pageType) {
         final Path potentialTemplateOverridePath = Path.of(
             pagePath.toString().replace(".adoc", ".html")
         );
         if (!Files.exists(potentialTemplateOverridePath)) {
-            return "page.html";
+            return "article".equals(pageType) ? "article.html" : "page.html";
         }
         return sourcePath.relativize(potentialTemplateOverridePath).toString();
     }
